51dd97ebc9c80ab593b237843d5753b44ab94c6c
Alexei Zubarik <alesha.zubarik@yandex.ru>
2018-09-26 22:36:48 +0300

    added convert rgb array to hsv array

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 window.onload = function() {
     let img = document.getElementById('img');
 
@@ -20,19 +21,58 @@ function getCanvasFromImg(img) {
 function getArrayImageData(canvas) {
     const ctx = canvas.getContext('2d');
     const arrayData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    const arrayRgb = getArrayRgb(arrayData);
+    const arrayHsv = getArrayHsv(arrayRgb);
 
-    return getArrayRGB(arrayData);
+    return [ arrayRgb, arrayHsv ];
 }
 
-function getArrayRGB(data) {
-    const arrayRGB = [];
+function getArrayRgb(data) {
+    const arrayRgb = [];
     
     data.forEach((element, i) => {
         if (i % 4 === 0) {
-            arrayRGB.push([]);
+            arrayRgb.push([]);
         }
-        arrayRGB[arrayRGB.length - 1].push(element);
+        arrayRgb[arrayRgb.length - 1].push(element);
     });
 
-    return arrayRGB;
+    return arrayRgb;
+}
+
+function getArrayHsv(arrayRgb) {
+    const arrayHsv = [];
+    
+    arrayRgb.forEach(color => {
+        arrayHsv.push(rgbToHsv(color[0], color[1], color[3]));
+    });
+
+    return arrayHsv;
+}
+
+function rgbToHsv(r, g, b) {
+    r /= 255;
+    g /= 255;
+    b /= 255;
+
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+    let h, s, v = max;
+
+    let d = max - min;
+    s = max == 0 ? 0 : d / max;
+
+    if (max == min) {
+        h = 0; // achromatic
+    } else {
+        switch (max) {
+            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+            case g: h = (b - r) / d + 2; break;
+            case b: h = (r - g) / d + 4; break;
+        }
+
+        h /= 6;
+    }
+
+    return [ h, s, v ];
 }
